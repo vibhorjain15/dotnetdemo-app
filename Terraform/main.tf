eeeb5a19f51ccf0405e9abd4c671780a33732f31
vibhorjain15 <87907103+vibhorjain15@users.noreply.github.com>
2023-07-28 15:35:05 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,6 +11,15 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+backend "s3" {
+   bucket = "wellness-360-tfstate"
+   key = "./terraform.tfstate"
+   region = "us-west-2"
+   dynamodb_table = "wellness-tfstate-table"
+   encrypt = true
+  }  
+}
+
 resource "tls_private_key" "pk" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -44,4 +53,67 @@ sudo yum install docker -y
 sudo systemctl enable docker --now
 
 EOF
+}
+
+# DynamoDB table for Terraform state locking
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "wellness-tfstate-table"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "wellness-tfstate-table"
+    Environment = "dev"
+  }
+}
+
+# S3 bucket resources
+resource "aws_s3_bucket" "bucket" {
+  count  = length(var.s3_bucket)
+  bucket = element(var.s3_bucket,count.index)
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_acl" "Wellness-acl" {
+  count  = length(var.s3_bucket)
+  bucket = element(aws_s3_bucket.bucket.*.id,count.index)
+  acl    = var.s3_acl
+
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  count  = length(var.s3_bucket)
+  bucket = element(aws_s3_bucket.bucket.*.id,count.index)
+
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_Wellness" {
+  count  = length(var.s3_bucket)
+  bucket = element(aws_s3_bucket.bucket.*.id,count.index)
+
+  versioning_configuration {
+    status = var.s3_bucket_versioning
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  count                    = length(var.s3_bucket)
+  bucket                   = element(aws_s3_bucket.bucket.*.id,count.index)
+  block_public_acls        = var.s3_bucket_block_acl
+  block_public_policy      = var.s3_bucket_block_public_policy
+  ignore_public_acls       = var.s3_bucket_block_public_acls
+  restrict_public_buckets  = var.s3_bucket_restrict
 }
